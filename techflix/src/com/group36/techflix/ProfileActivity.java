04c447748fe6639519eadc92e5384128672552d9
Omar Sharifali <osharifali@gatech.edu>
2016-01-25 13:26:27 -0500

    Made a profile activity and a registration activity

---
+++ techflix/src/com/group36/techflix/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.group36.techflix;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by osharifali on 1/25/16.
+ */
+public class ProfileActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
