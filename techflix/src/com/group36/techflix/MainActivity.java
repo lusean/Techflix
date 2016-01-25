2d62a6ea2ce4e3b04961fc3fcc389776faf06553
Omar Sharifali <osharifali@gmail.com>
2016-01-25 13:51:59 -0500

    Added a login activity/screen also

--- techflix/src/com/group36/techflix/MainActivity.java
+++ techflix/src/com/group36/techflix/MainActivity.java
@@ -3,6 +3,8 @@ package com.group36.techflix;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -16,8 +18,13 @@ public class MainActivity extends Activity {
 
     }
 
-    public void allowRegistration(Button button) {
+    public void allowRegistration(View view) {
         Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
