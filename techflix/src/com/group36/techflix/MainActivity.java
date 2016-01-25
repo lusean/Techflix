b57c7a3a9bd0c5a27c09918547efc9e08f587342
Omar Sharifali <osharifali@gmail.com>
2016-01-25 13:43:18 -0500

    Added a login screen which allows you to go to the registration/login screen

--- techflix/src/com/group36/techflix/MainActivity.java
+++ techflix/src/com/group36/techflix/MainActivity.java
@@ -1,7 +1,9 @@
 package com.group36.techflix;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +13,11 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+    }
+
+    public void allowRegistration(Button button) {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
     }
 }
