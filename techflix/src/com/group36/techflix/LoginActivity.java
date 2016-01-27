79a9eb8e57983d49e7c001d24c3b89fca59c7906
Developer <developer@lawn-143-215-121-11.lawn.gatech.edu>
2016-01-27 13:53:17 -0500

    username and password check functionality

--- techflix/src/com/group36/techflix/LoginActivity.java
+++ techflix/src/com/group36/techflix/LoginActivity.java
@@ -1,23 +1,41 @@
 package com.group36.techflix;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by osharifali on 1/27/16.
  */
 public class LoginActivity extends Activity {
+    EditText username;
+    EditText password;
+    final String CORRECT_USERNAME = "admin";
+    final String CORRECT_PASSWORD = "password";
+    boolean correctInput;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
     }
 
     //HRISHEEK FILL IN THIS CODE
     public void checkCredentials(View view) {
         // get info from username text field and check
-        // get ifo from password text field and check
+        String usernameInput = username.getText().toString();
+        // get info from password text field and check
+        String passwordInput = password.getText().toString();
         // put a dialog also if the password is incorrect
+        if (usernameInput.equals(CORRECT_USERNAME) && passwordInput.equals(CORRECT_PASSWORD)) {
+
+        } else {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("Incorrect Username or Password.");
+        }
     }
 }
