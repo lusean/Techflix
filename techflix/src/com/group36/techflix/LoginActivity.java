25175c6b3f34dd513f92a040543e73b0a835b42f
Scott <Scott>
2016-02-03 14:31:40 -0500

    Hope this works

---
+++ techflix/src/com/group36/techflix/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.group36.techflix;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.content.DialogInterface;
+
+/**
+ * Created by osharifali on 1/27/16.
+ */
+public class LoginActivity extends Activity {
+    EditText username;
+    EditText password;
+    final String CORRECT_USERNAME = "admin";
+    final String CORRECT_PASSWORD = "password";
+    boolean correctInput;
+    private Button loginButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        loginButton = (Button)findViewById(R.id.appLogin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (v == loginButton) {
+                    checkCredentials(loginButton);
+                }
+            }
+        });
+    }
+
+    //HRISHEEK FILL IN THIS CODE
+    public void checkCredentials(View view) {
+        // get info from username text field and check
+        String usernameInput = username.getText().toString();
+        // get info from password text field and check
+        String passwordInput = password.getText().toString();
+        // put a dialog also if the password is incorrect
+        if (usernameInput.equals(CORRECT_USERNAME) && passwordInput.equals(CORRECT_PASSWORD)) {
+            Intent profile = new Intent(this, ProfileActivity.class);
+            startActivity(profile);
+        } else {
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Error");
+            alertDialog.setMessage("The Username or Password was incorrect.");
+            alertDialog.setButton(DialogInterface.BUTTON_NEUTRAL, "Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    alertDialog.dismiss();
+                }
+            });
+            alertDialog.show();
+        }
+    }
+}
---
+++ techflix/src/com/group36/techflix/MainActivity.java
@@ -0,0 +1,33 @@
+package com.group36.techflix;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+
+    public void appLogin(View view) {
+        Intent login = new Intent(this, LoginActivity.class);
+        Log.d("LOGIN", "Intent launched.");
+        startActivity(login);
+    }
+
+    public void allowRegistration(View view) {
+        Intent register = new Intent(this, RegistrationActivity.class);
+        Log.d("REGISTER", "Intent launched.");
+        startActivity(register);
+    }
+
+}
