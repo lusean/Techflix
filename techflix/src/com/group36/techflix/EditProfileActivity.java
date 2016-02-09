48948a27a7d6bff5be2f66bdd08b0e50ba4c6167
Omar Sharifali <osharifali@gatech.edu>
2016-02-09 16:07:14 -0500

    Added an edit button and edit profile activity

---
+++ techflix/src/com/group36/techflix/EditProfileActivity.java
@@ -0,0 +1,15 @@
+package com.group36.techflix;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by osharifali on 2/9/16.
+ */
+public class EditProfileActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editprofile);
+    }
+}
