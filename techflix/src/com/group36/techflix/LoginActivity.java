16053da02a617ad90dffe92c31b7b53d5957bb77
Scott <Scott>
2016-02-17 21:36:04 -0500

    Added admin login

--- techflix/src/com/group36/techflix/LoginActivity.java
+++ techflix/src/com/group36/techflix/LoginActivity.java
@@ -18,6 +18,7 @@ import android.content.Intent;
 public class LoginActivity extends Activity {
     EditText username;
     EditText password;
+    EditText adminCode;
     final String CORRECT_USERNAME = "admin";
     final String CORRECT_PASSWORD = "password";
     boolean correctInput;
@@ -29,6 +30,7 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.login);
         username = (EditText)findViewById(R.id.username);
         password = (EditText)findViewById(R.id.password);
+        adminCode = (EditText)findViewById(R.id.admincode);
         loginButton = (Button)findViewById(R.id.appLogin);
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -54,11 +56,22 @@ public class LoginActivity extends Activity {
     public void checkCredentials(View view) {
         String usernameInput = username.getText().toString();
         String passwordInput = password.getText().toString();
+        String adminInput = adminCode.getText().toString();
         Authentication loginHandler = new UserManager();
 
         CharSequence toastText;
-        if (loginHandler.executeLogin(usernameInput, passwordInput)) {
-            toastText = "Login Success";
+        User user = ((UserManager)loginHandler).findUserByUsername(usernameInput);
+        if (loginHandler.executeLogin(usernameInput, passwordInput, user)) {
+            Intent intent;
+            if (((UserManager)loginHandler).getAdminCode().equals(adminInput)) {
+                toastText = "Admin Login Success";
+                user.setAdminStatus(true);
+                intent = new Intent(this, UserListActivity.class);
+            } else {
+                toastText = "Login Success";
+                user.setAdminStatus(false);
+                intent = new Intent(this, ProfileActivity.class);
+            }
             Context context = getApplicationContext();
             int duration = Toast.LENGTH_SHORT;
             Toast t = Toast.makeText(context, toastText, duration);
