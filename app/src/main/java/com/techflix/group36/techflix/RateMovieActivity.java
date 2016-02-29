9ea382f7746ab902be821b40b28beb2cadec2b13
Akshay Easwaran <akeaswaran@me.com>
2016-02-28 23:49:04 -0500

    removed complile-breaking extra code

--- app/src/main/java/com/techflix/group36/techflix/RateMovieActivity.java
+++ app/src/main/java/com/techflix/group36/techflix/RateMovieActivity.java
@@ -23,18 +23,6 @@ public class RateMovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate_movie);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         movieTitleView = (TextView)findViewById(R.id.movieTitleView);
         Bundle extras = getIntent().getExtras();
