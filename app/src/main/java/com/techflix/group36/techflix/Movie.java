8a586119a2b1d1f18a18899585929abe16abd2f5
Sean Lu <slu81@gatech.edu>
2016-03-06 16:04:05 -0500

    Getting recommendation movie lists
    List for movies in order of rating, top rated, and top rated by major

--- app/src/main/java/com/techflix/group36/techflix/Movie.java
+++ app/src/main/java/com/techflix/group36/techflix/Movie.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by osharifali on 2/24/16.
@@ -16,6 +17,7 @@ public class Movie implements Serializable {
     private String title;
     private String mpaaRating;
     private String year;
+    private ArrayList<Movie> ratedMovies = new ArrayList<Movie>();
 
 
     public Movie(String title, String year, String mpaaRating) {
@@ -42,5 +44,86 @@ public class Movie implements Serializable {
     public void rateMovie(float stars, String comment) {
         Rating newRating = new Rating(stars, comment, User.currentUser, this);
         newRating.save();
+        if (!ratedMovies.contains(this)) {
+            ratedMovies.add(this);
+        }
+    }
+
+    public float getRatingAvg(ArrayList<Rating> list) {
+        float total = 0;
+        float count = 0;
+        for (Rating rating: list) {
+            total += rating.getStars();
+            count++;
+        }
+        return total / count;
+    }
+
+    //Puts movies in order of best rating
+    public ArrayList<Movie> filterMoviesByRating() {
+        ArrayList<Movie> results = new ArrayList<Movie>();
+        HashMap<Float, Movie> avgRatings = new HashMap<Float, Movie>();
+        for (Movie movie: ratedMovies) {
+            float rating = getRatingAvg(movie.getRatings());
+            avgRatings.put(rating, movie);
+        }
+        while (!avgRatings.isEmpty()) {
+            float max = 0;
+            for (float rating: avgRatings.keySet()) {
+                if (rating > max) {
+                    max = rating;
+                }
+                results.add(avgRatings.get(max));
+                avgRatings.remove(max);
+            }
+        }
+        return results;
+    }
+
+    //top 5 movies, must have 3+ recs
+    public ArrayList<Movie> filterTopMoviesByRating() {
+        ArrayList<Movie> results = new ArrayList<Movie>();
+        results = filterMoviesByRating();
+        int count = 0;
+        while (count < 5) {
+            if (results.get(count).getRatings().size() >= 3) {
+                count++;
+            } else {
+                results.remove(count);
+            }
+        }
+        return results;
+    }
+
+    //top 5 movies w/ major, must have 3+ recs
+    public ArrayList<Movie> filterMoviesByMajor(String major) {
+        ArrayList<Movie> results = new ArrayList<Movie>();
+        HashMap<Float, Movie> avgRatings = new HashMap<Float, Movie>();
+        for (Movie movie: ratedMovies) {
+            int majorCount = 0;
+            ArrayList<Rating> list = movie.getRatings();
+            for (Rating rating: list) {
+                if (rating.getAuthor().getMajor().equals(major)) {
+                    majorCount++;
+                } else {
+                    list.remove(rating);
+                }
+            }
+            if (majorCount >= 3) {
+                float rating = getRatingAvg(list);
+                avgRatings.put(rating, movie);
+            }
+        }
+        while (!avgRatings.isEmpty()) {
+            float max = 0;
+            for (float rating: avgRatings.keySet()) {
+                if (rating > max) {
+                    max = rating;
+                }
+                results.add(avgRatings.get(max));
+                avgRatings.remove(max);
+            }
+        }
+        return results;
     }
 }
