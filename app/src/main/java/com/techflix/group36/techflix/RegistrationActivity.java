a0ce5aa062eae08d2ae8a4c6ac8be3a36f03c274
Scott <smessing3@gatech.edu>
2016-03-11 21:03:09 -0500

    Properly displays error msg for registering with taken Username

--- app/src/main/java/com/techflix/group36/techflix/RegistrationActivity.java
+++ app/src/main/java/com/techflix/group36/techflix/RegistrationActivity.java
@@ -91,6 +91,7 @@ public class RegistrationActivity extends Activity {
                         alertDialog.dismiss();
                     }
                 });
+                alertDialog.show();
             }
         }
     }
